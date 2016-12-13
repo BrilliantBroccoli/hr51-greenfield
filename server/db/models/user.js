603e6dc8e3023e975b06e2bc27b599f643539733
sean-oneal <smoneal@gmail.com>
2016-12-12 17:07:34 -0800

    Add Geolocation and Final Structure to App

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -2,8 +2,6 @@ const mongoose = require('mongoose');
 mongoose.Promise = Promise;
 
 const userSchema = new mongoose.Schema({
-  name: String,
-  age: Number,
   IP: String,
   category: String
 });
